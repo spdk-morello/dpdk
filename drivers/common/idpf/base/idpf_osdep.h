34d5b882bb075b5758cde387942c11b4901f2194
Nick Connolly <47530082+nconnolly1@users.noreply.github.com>
2023-02-18 19:26:06 +0000

    Remove packed annotation from structure
    The structure does not appear to be visible externally, which suggests
    the pack attribute is purely for memory efficiency. Marked as HACK
    because this needs to be verified.
    
    Signed-off-by: Nick Connolly <47530082+nconnolly1@users.noreply.github.com>

--- drivers/common/idpf/base/idpf_osdep.h
+++ drivers/common/idpf/base/idpf_osdep.h
@@ -175,12 +175,20 @@ struct idpf_dma_mem {
 	u64 pa;
 	u32 size;
 	const void *zone;
+#ifndef RTE_ARCH_ARM_PURECAP_HACK
 } __rte_packed;
+#else
+};
+#endif
 
 struct idpf_virt_mem {
 	void *va;
 	u32 size;
+#ifndef RTE_ARCH_ARM_PURECAP_HACK
 } __rte_packed;
+#else
+};
+#endif
 
 #define idpf_malloc(h, s)	rte_zmalloc(NULL, s, 0)
 #define idpf_calloc(h, c, s)	rte_zmalloc(NULL, (c) * (s), 0)
